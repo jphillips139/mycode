2326ceb285db293e1ac9f6e54d8fb416b19299d3
Marshall Bargar <mbargar@selectsires.com>
2016-02-06 21:29:23 +0000

    Changes made by Marshall Bargar (mbargar@selectsires.com).

--- ProjectFalcon/app/cowlist.js
+++ ProjectFalcon/app/cowlist.js
@@ -1,11 +1,70 @@
+var config = require("./shared/models/config");
+var el = require("./shared/models/el");
+var httpModule = require("http");
 var dialogs = require("ui/dialogs");
 var application = require("application");
 var frameModule = require("ui/frame");
 var viewModule = require("ui/core/view");
 var buttonModule = require("ui/button");
-var observable = require("data/observable");
-var httpModule = require("http");
+var observableModule = require("data/observable");
+var observableArray = require("data/observable-array");
+
 var page;
+var pageData = new observableModule.Observable();
+var cows = new observableArray.ObservableArray([]);
+
+exports.load = function (args) {
+    page = args.object;
+    pageData.set("cow", "");
+    pageData.set("cows", cows);
+    page.bindingContext = pageData;
+    // Empty the array for subsequent visits to the page
+    while (cows.length) {
+        cows.pop();
+    }
+    loadCow();
+};
+
+function loadCow() {
+    el.data("Cow").get().then(function (data) {
+        data.result.forEach(function (cow) {
+            cows.push({
+                name: cow.CowID
+            });
+        });
+    });
+}
+
+exports.cowTraitNavigate = function (args) {
+ var navigationEntry = {
+      moduleName : "./cowtrait",
+      context : {
+              cowId : args.object.bindingContext.id
+      }
+};
+
+frameModule.topmost().navigate(navigationEntry);
+};
+
+exports.addCow = function () {
+    // Dismiss the keyboard before adding to the list
+    viewModule.getViewById(page, "cow").dismissSoftInput();
+
+    addCow(pageData.get("cow"));
+
+    // Clear the text field
+    pageData.set("cow", "");
+};
+
+function addCow(cow) {
+    el.data("Cow").create({
+        Name: cow
+    }).then(function (result) {
+        cows.push({
+            name: cow.CowID
+        });
+    });
+}
 
 exports.cowedit = function (args) {
      frameModule.topmost().navigate("./cowedit");
@@ -18,4 +77,3 @@ exports.cowtraits = function (args) {
 exports.herdlist = function (args) {
      frameModule.topmost().navigate("./herdlist");
 };
-
